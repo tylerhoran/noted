476e49d0354de62e3301e49eaee56f87705d9ce3
Tyler <tylerhoran@gmail.com>
2020-02-13 05:21:38 -0800

    adding note processign

--- app/mailboxes/processor_mailbox.rb
+++ app/mailboxes/processor_mailbox.rb
@@ -2,7 +2,8 @@ class ProcessorMailbox < ApplicationMailbox
   def process
     return if user.nil?
     folder = user.folders.find_by(name: 'Inbox')
-    user.notes.create(text: mail.parts[0].body.decoded, folder: folder)
+    note = user.notes.create(text: mail.parts[0].body.decoded, folder: folder)
+    ProcessEntities.perform_async(note.id)
   end
 
   def user
