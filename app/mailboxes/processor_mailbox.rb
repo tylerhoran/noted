736568db89807c93213755b968155dd86a29d911
Tyler <tylerhoran@gmail.com>
2020-02-13 05:30:13 -0800

    adding mail decoded

--- app/mailboxes/processor_mailbox.rb
+++ app/mailboxes/processor_mailbox.rb
@@ -2,7 +2,7 @@ class ProcessorMailbox < ApplicationMailbox
   def process
     return if user.nil?
     folder = user.folders.find_by(name: 'Inbox')
-    note = folder.notes.create(text: mail.parts[0].body.decoded, folder: folder)
+    note = folder.notes.create(text: mail.decoded, folder: folder)
     ProcessEntities.perform_async(note.id)
   end
 
